45034623973b3f0e268843c9daf3107511a67f7e
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-04-06 19:17:53 -0300

    Create scripts.js

---
+++ curso_js/js/scripts.js
@@ -0,0 +1,20 @@
+/* Aula 3 Variaveis.
+console.log('Hello Mundo!');
+alert('Hello Mundo');
+
+var latitude = 40.87663;
+var longitude = -8.08373;
+
+
+console.log(latitude); */
+
+var nome = "João";  // podem ser usadas aspas duplas
+    var sobrenome = 'Gomes';  // ou aspas simples
+    var cpf = '111.111.111-11';  
+    var telefone = "998887655";
+    var ddd = '21';
+    var email = 'joao@gmail.com';
+    var biografia = "João começou a sua carreira como desenvolvedor web em 2010 e já participou de importantes projetos, como...";
+    
+    console.log(nome + ' ' + sobrenome );
+    console.log(`Nome completo : ${nome+' '+sobrenome}`);
