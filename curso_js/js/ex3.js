2667af5ebb7813405ce46a5c6ac33a39af0fdbba
Gabriel Melo <gabrielmelo64@gmail.com>
2023-04-20 18:37:38 -0300

    Update ex3.js

--- curso_js/js/ex3.js
+++ curso_js/js/ex3.js
@@ -1,7 +1,5 @@
-// Exercicios 
-/* 1) Use a função para converter de Celsius para Fahrenheit feita no exercício 2. Só que desta vez, a temperatura em Celsius deve ser digitada pelo usuário no campo apropriado e o cálculo deve ser feito quando o botão "Calcular" for pressionado.
+// Exercicio 1 
 
-* Para pegar o valor do input de temperatura use .value em vez de .innerHTML */
 
 /* document.getElementById('converter').onclick = 
 function fahrenheit() {
@@ -10,14 +8,8 @@ function fahrenheit() {
     document.getElementById('temp_fahr').innerHTML = fahrenheit;
     
 } */
+//Exercicio 2
 
-/*  
-2) Solte dentro do elemento abaixo 
-uma lista de todos os anos em que houve a Copa 
-do Mundo de Futebol, desde 1930 até 2018. Lembre-se 
-que a copa do mundo ocorre de 4 em 4 anos.
-
-*/
 /* for ( var ano = 1926; ano < 2018; ano + 4){
     var copa = "<li>" + ano[i] + "</li>";
     document.getElementById("anos_copa").innerHTML = ano;
@@ -28,19 +20,9 @@ que a copa do mundo ocorre de 4 em 4 anos.
     document.getElementById('anos_copa').innerHTML = anos_copa;
 } */
 
-/* 3) Informe nos campos abaixo duas notas e o total de faltas do aluno e depois solte o resultado
- no local apropriado. Sendo que:
+// Exercicio 3.
 
-O aluno para ser aprovado precisa ter, 
-no mínimo 70% de presença (o total de
-     aulas é 20)
-O aluno para ser aprovado precisa ter 
-média maior ou igual a 6.5
-Caso o aluno não seja aprovado, o programa precisa dizer se foi por motivo de média insuficiente, por faltas ou pelos dois motivos.
-
-*/
-
-document.getElementById('calcular').onclick = function(){
+/* document.getElementById('calcular').onclick = function(){
     var n1 = Number(document.getElementById('nota1').value);
     var n2 = Number(document.getElementById('nota2').value);
     var faltas = document.getElementById('n_faltas').value;
@@ -56,4 +38,8 @@ document.getElementById('calcular').onclick = function(){
         }
     }
     document.getElementById('result').innerHTML = res;
-}
+} */
+
+// Exercicio 4.
+
+
