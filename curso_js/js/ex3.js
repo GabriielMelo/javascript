90fcf31a7fd6a74181cab4c5cb3ac45d001d34d9
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-04-20 09:18:38 -0300

    Update ex3.js

--- curso_js/js/ex3.js
+++ curso_js/js/ex3.js
@@ -18,10 +18,15 @@ do Mundo de Futebol, desde 1930 até 2018. Lembre-se
 que a copa do mundo ocorre de 4 em 4 anos.
 
 */
-for (var anos_copa = 1926; anos_copa < 2018;) {
+for ( var ano = 1926; ano < 2018; ano + 4){
+    var copa = "<li>" + ano[i] + "</li>";
+    document.getElementById("anos_copa").innerHTML = ano;
+}    
+
+/* for (var anos_copa = 1926; anos_copa < 2018;) {
     anos_copa += 4;
     document.getElementById('anos_copa').innerHTML = anos_copa;
-}
+} */
 
 /* 3) Informe nos campos abaixo duas notas e o total de faltas do aluno e depois solte o resultado
  no local apropriado. Sendo que:
@@ -35,11 +40,11 @@ Caso o aluno não seja aprovado, o programa precisa dizer se foi por motivo de m
 
 */
 
-var n1 = document.getElementById('nota1').value;
+/* var n1 = document.getElementById('nota1').value;
 var n2 = document.getElementById('nota2').value;
 var falta = document.getElementById('n_faltas').value;
 var media = (n1+n2)/2;
 
 if();
 
-// id 'result';
+// id 'result'; */
