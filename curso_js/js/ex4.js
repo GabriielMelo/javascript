0ce71e430b0a1df129ea3f42f303c85e690cf73f
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-04-23 21:50:59 -0300

    Update ex4.js

---
+++ curso_js/js/ex4.js
@@ -0,0 +1,31 @@
+// Exercicio 1 OK
+
+/* document.getElementById('estados'), onchange = function () {
+
+    var estado = document.getElementById('estados').value;
+    localStorage.setItem('estado', estado);
+}
+document.getElementById('estados').value = localStorage['estado'];
+ */
+//----------------------------------------------
+
+// Exercicio 2.
+
+document.getElementById('confirmar_pedido').onclick = function () {
+
+    var pedido = new Date();
+    pedido = pedido.getTime();
+
+    var entrega_express = 12 * 86400000;
+    var entrega_normal = 18 * 86400000;
+    var entrega = document.getElementById('envios').value;
+    switch(entrega){
+        case "normal" : 
+        pedido += entrega_normal
+        break;
+        case "express" : 
+        pedido += entrega_express;
+        break;
+    }
+    console.log(pedido)
+}
