8701e096aeb547a509e2702158d7334d8882c465
Gabriel Melo <gabrielmelo64@gmail.com>
2023-04-25 17:48:47 -0300

    Update ex4.js

--- curso_js/js/ex4.js
+++ curso_js/js/ex4.js
@@ -1,5 +1,5 @@
 // Exercicio 1 OK
-
+//Utilizando o evento onchange para salvar no local storage.
 /* document.getElementById('estados'), onchange = function () {
 
     var estado = document.getElementById('estados').value;
@@ -14,20 +14,37 @@ document.getElementById('estados').value = localStorage['estado'];
 document.getElementById('confirmar_pedido').onclick = function () {
 
     var pedido = new Date();
+    var pedido_dia = pedido.getDate();
+    if (pedido_dia > 0 && pedido_dia < 9) {
+        pedido_dia = "0" + pedido_dia.toString();
+    }
+    var pedido_mes = pedido.getMonth();
+    if (pedido_mes > 0 && pedido_mes < 9) {
+        pedido_mes = "0" + pedido_mes.toString();
+    }
+    var pedido_ano = pedido.getFullYear();
     pedido = pedido.getTime();
-
-    var entrega_express = 12 * 86400000;
-    var entrega_normal = 18 * 86400000;
     var entrega = document.getElementById('envios').value;
-    switch(entrega){
-        case "normal" : 
-        pedido += entrega_normal
-        break;
-        case "express" : 
-        pedido += entrega_express;
-        break;
+    switch (entrega) {
+        case "normal":
+            pedido = pedido + (18 * 86400000);
+            break;
+        case "express":
+            pedido = pedido + (12 * 86400000);
+            break;
     }
     var data_entrega = new Date(pedido);
-    
-    console.log(dia_entrega);
+    var dia_entrega = data_entrega.getDate();
+    if (dia_entrega > 0 && dia_entrega < 9) {
+        dia_entrega = "0" + dia_entrega.toString();
+    }
+    var mes_entrega = data_entrega.getMonth();
+    if (mes_entrega > 0 && mes_entrega < 9) {
+        mes_entrega = "0" + mes_entrega.toString();
+    }
+    var ano_entrega = data_entrega.getFullYear();
+    tipo_entrega = document.getElementById('envios').value;
+    document.getElementById('data_pedido').innerHTML = `${pedido_dia}/${pedido_mes}/${pedido_ano} Entrega: ${tipo_entrega}`;
+    document.getElementById('data_entrega').innerHTML = `${dia_entrega}/${mes_entrega}/${ano_entrega}`;
+
 }
