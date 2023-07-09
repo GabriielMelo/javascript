647cb3303743159473465e15e00d16474c0d1274
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-07-09 11:03:27 -0300

    algumas requisições estão concluídas.

--- curso_js/weather_app/js/scripts.js
+++ curso_js/weather_app/js/scripts.js
@@ -1,59 +1,59 @@
 
-// $(function(){
 
 
-// *** APIs ***
-// clima, previsão 12 horas e previsão 5 dias: https://developer.accuweather.com/apis
-// pegar coordenadas geográficas pelo nome da cidade: https://docs.mapbox.com/api/
-// pegar coordenadas do IP: http://www.geoplugin.net
-// gerar gráficos em JS: https://www.highcharts.com/demo
 
-// $.ajax({
-//     url : "http://dataservice.accuweather.com/currentconditions/v1/28143?apikey=kQ94WGEKAe7Ae968qc1ehnYVqfqbhDbq&language=pt-br",
-//     type: "GET",
-//     dataType: "json",
-//     sucess: function(data){
-//         console.log(data);
-//     },
-//     error: function(){
-//         console.log("Erro na requisição")
-//     }
-    
-// });
-
-// });
-
+// ********** Requisições ***********
 var accWeatherApiKey = "kQ94WGEKAe7Ae968qc1ehnYVqfqbhDbq";
 
-async function localCode(localCode,lat,long){
+async function pegarTempoAtual(localCode) {
+    try {
+        const resposta = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${localCode}?apikey=${accWeatherApiKey}&language=pt-br`)
+        var data = await resposta.json();
+        console.log(data);
+    } catch (error) {
 
-
-
-    try{
-        var localCode = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey="${accWeatherApiKey}"=${lat}%2C%20${long}&language=pt-br`);
-       getWeather();
-    }catch (error){
-        console.log('Erro na requisição', error);
-        throw error ; 
+        console.log('Erro na requisição')
     }
 
- 
-
-async function getWeather(){
-    try{
-
-        const response = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/"${localCode}"?apikey=${accWeatherApiKey}&language=pt-br`);
-        const dados = await response.json();
-        console.log(dados);
-        return dados;
-        
-    }
-    catch ( error){
-        console.log('Erro na requisição', error);
-        throw error ; 
-    }
-    
-}
 }
 
-localCode(-23.489387,-46.436476);
+// A função pegar local irá aguardar resposta da função coordIP para obter os dados da latitude e longitude de acordo com o endereço de ip.
+
+async function pegarLocal(lat, long) {
+    try {
+        const resposta = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${accWeatherApiKey}&q=${lat}%2C%20${long}&language=pt-br`)
+        data = await resposta.json();
+        var localCode = data.Key;
+        await pegarTempoAtual(localCode);
+
+
+    } catch (error) {
+
+        console.log('Erro na requisição')
+    }
+}
+// pegarLocal(-23.59572, -46.34079);
+// Função identifica o IP do usuário e dentro do objeto iremos obter a latitude e longitude de acordo com o endereço de IP, nisso chamamos a função passando os parametros obtidos pela função pegarCoordIP onde será executado a função pegarLocal.
+
+async function pegarCoordIP() {
+    var lat_padrao = -23.59572; // Em caso de bloqueio de ip
+    var long_padrao = -46.34079; // em caso de bloqueio de ip.
+    try {
+
+        const resposta = await fetch(`http://www.geoplugin.net/json.gp`)
+        var data = await resposta.json();
+
+        if (data.geoplugin_latitude && data.geoplugin_longitude) {
+            await pegarLocal(data.geoplugin_latitude, data.geoplugin_longitude)
+        } else {
+            pegarLocal(lat_padrao, long_padrao);
+        };
+    } catch (error) {
+
+        console.log('Erro na requisição')
+        pegarLocal(lat_padrao, long_padrao);
+    }
+}
+pegarCoordIP();
+//
+
