fa9abd72fd751ebac3ee0dcf6af51857c44b815b
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-07-21 09:03:48 -0300

    Update scripts.js

--- curso_js/weather_app/js/scripts.js
+++ curso_js/weather_app/js/scripts.js
@@ -1,3 +1,8 @@
+// *** APIs ***
+// clima, previsão 12 horas e previsão 5 dias: https://developer.accuweather.com/apis
+// pegar coordenadas geográficas pelo nome da cidade: https://docs.mapbox.com/api/
+// pegar coordenadas do IP: http://www.geoplugin.net
+// gerar gráficos em JS: https://www.highcharts.com/demo
 // ****************** Variaveis ******************
 
 var accWeatherApiKey = "kQ94WGEKAe7Ae968qc1ehnYVqfqbhDbq";
